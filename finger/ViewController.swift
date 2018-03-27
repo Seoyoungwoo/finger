adb842a2a1664723d4b2e12c5f49fc4a7465187e
D7703_17 <d7703_17@d7703-17ui-imac.local>
2018-03-27 15:50:09 +0900

    add finger game

--- finger/ViewController.swift
+++ finger/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+      @IBOutlet weak var txt: UITextField!
+      @IBOutlet weak var result: UILabel!
       override func viewDidLoad() {
             super.viewDidLoad()
             // Do any additional setup after loading the view, typically from a nib.
+            txt.delegate = self
       }
+      
 
       override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
       }
 
-
+      @IBAction func guess(_ sender: Any) {
+            // 랜덤 숫자 생성
+            let number = arc4random_uniform(6)
+            print(number)
+            let numberString = String(number)
+            
+            if txt.text == numberString {
+                  result.text = "빙고!!"
+            }
+            else {
+                  result.text = "실패!! Try again!! \n 숨겨진 숫자는 현재 \(number)"
+            }
+      }
+      
 }
 
